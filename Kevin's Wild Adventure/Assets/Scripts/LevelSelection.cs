ab3879283e32405464ccf93fc52f4937be163a7f
alillo2 <alillo2@student.cvtc.edu>
2021-12-07 14:20:26 -0600

    reset game back to lock levels again, add a commented out statement to reset level unlock in levelSelection script

--- Kevin's Wild Adventure/Assets/Scripts/LevelSelection.cs
+++ Kevin's Wild Adventure/Assets/Scripts/LevelSelection.cs
@@ -10,6 +10,11 @@ public class LevelSelection : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        // This commented out statement will reset player levels back to the start
+        // meaning any levels the player unlocked are locked again
+        // uncomment this whenever you would like to lock levels again.
+        // Make sure to comment this back out after you open the levels scene.
+        //PlayerPrefs.DeleteKey("levelAt");
         int levelAt = PlayerPrefs.GetInt("levelAt", 2); /* < Change this int value to whatever your
                                                              level selection build index is on your
                                                              build settings */
