844ad8da5097dc1902305d3a7557345048b98537
jfrideres <jakefrideres@gmail.com>
2021-12-10 16:24:43 -0600

    This commit will add sound effects to the checkpoints

--- Kevin's Wild Adventure/Assets/Scripts/CheckpointController.cs
+++ Kevin's Wild Adventure/Assets/Scripts/CheckpointController.cs
@@ -4,12 +4,17 @@ using UnityEngine;
 
 public class CheckpointController : MonoBehaviour
 {
+
+    // Creates a field to set a sound for the checkpoint
+    [SerializeField] private AudioSource checkPointSoundEffect;
+
     public static Vector3 lastCheckPoint = new Vector3(-13, -2, 0);
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.transform.tag == "Player")
         {
+            checkPointSoundEffect.Play();
             lastCheckPoint = transform.position;
         } 
     }
